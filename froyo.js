9b47a6d1c9e077acc70dc5000f75e689fcb4b976
jayceforte <jayce4tay@gmail.com>
2024-12-18 19:17:47 -0700

    edited js file

--- froyo.js
+++ froyo.js
@@ -6,5 +6,32 @@
 // INPUT: vanilla,vanilla,vanilla,strawberry,coffee,coffee
 // OUTPUT{vanilla: 3, strawberry: 1, coffee: 2,}
 
-let flaows = window.prompt("Enter Your Flavors Below")
-console.log(flavors)
+let Flavors = window.prompt("Enter Your Flavors Below")
+
+const Vanilla = {
+    name:"Vanilla",
+    isInStock:true,
+    Flavors:["Vanilla"],
+}
+const key = "isInStock"
+Vanilla.amount = "3"
+console.log(Vanilla)
+
+const Strawberry = {
+    name:"Strawberry",
+    isInStock:true,
+    Flavors:["Strawberry"],
+}
+const key1 = "isInStock"
+Strawberry.amount = "1"
+console.log(Strawberry)
+
+const Coffee = {
+    name: "Coffee",
+    isInStock:true,
+    Flavors:["Coffee"]
+}
+const key2 = "isInStock"
+Coffee.amount = "2"
+console.log(Coffee)
+console.log(Vanilla.isInStock)
